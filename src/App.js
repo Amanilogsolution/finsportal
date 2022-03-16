90605e8df062d68380dee335323042e75fbc1ab7
Amanilogsolution <aman@ilogsolution.com>
2022-03-16 10:44:14 +0530

    push

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Login from './Login'
+import Home from './Home'
+import Customer from './Customer'
+import Organisation from './organisation/Organisation'
+import Org from './organisation/org'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/Customer" element={<Customer/>}/>
+          <Route path="/Organisation" element={<Organisation/>}/>
+          <Route path="/org" element={<Org/>}/>
+          </Routes>
+        
+        </BrowserRouter>
+        
+      </div>
+    )
+  }
+}
