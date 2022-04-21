9717da2a6803acf24cdb8e78dc503995f4961b8f
rupeshilogsolution <rupesh.kumar@ilogsolution.com>
2022-04-21 17:46:24 +0530

    Hideand Show password

--- Frontend/src/component/Login/Login.js
+++ Frontend/src/component/Login/Login.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react'
+import React, {useState } from 'react'
 import './login.css'
 import {UserLogin} from '../../api'
 
 
 const Login = () => {
+  const [passwordshow, setPasswordshow] = useState(false);
+  const handleClickToogle = (e) => {
+    e.preventDefault()
+    setPasswordshow(!passwordshow)
+  }
+
     const handleClick = async (e) => {
         e.preventDefault()
         const email = document.getElementById('email').value
@@ -16,7 +22,7 @@ const Login = () => {
               window.location.href = '/home'
           }
           else{
-            alert('Invalid Credentials')
+            alert(<span style={{color:'red'}}>{`Invalid Email & Password`}</span>)
         }
     }
 
@@ -39,30 +45,34 @@ const Login = () => {
                                 </div>
                             </div>
                             <div className="input-group mb-3">
-                                <input type="password" className="form-control" placeholder="Password" id="password" required/>
+                                <input type={passwordshow ? "text" : "password"}  className="form-control" placeholder="Password" id="password" required/>
                                 <div className="input-group-append">
+                            <span className="input-group-text" onClick={handleClickToogle}>{passwordshow ?  <i className="fa fa-eye" aria-hidden="true"></i>:<i className="fa fa-eye-slash" aria-hidden="true"></i> }</span>
+                          </div>
+                                {/* <div className="input-group-append">
                                     <div className="input-group-text">
                                         <span className="fas fa-lock" />
                                     </div>
-                                </div>
+                                </div> */}
                             </div>
                             <div className="row">
-                                <div className="col-8">
+                                {/* <div className="col-8">
                                     <div className="icheck-primary">
                                         <input type="checkbox" id="remember" />
                                         <label htmlFor="remember">
                                             Remember Me
                                         </label>
                                     </div>
-                                </div>
-                                <div className="col-4">
+                                </div> */}
+                                <div className="col-4" style={{marginLeft:"50%",transform:"translate(-50%)"}}>
                                     <button type="submit" onClick={handleClick} className="btn btn-primary btn-block">Sign In</button>
+                                    
                                 </div>
                             </div>
                         </form>
-                        <p className="mb-1">
+                        {/* <p className="mb-1">
                             <a href="forgot-password.html">I forgot my password</a>
-                        </p>
+                        </p> */}
                     </div>
                 </div>
             </div>
