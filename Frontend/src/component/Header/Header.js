4dbc4e32ca4a484ad2369d4642cdb9db7f57a1d6
rupeshilogsolution <rupesh.kumar@ilogsolution.com>
2022-05-07 11:36:59 +0530

    db

--- Frontend/src/component/Header/Header.js
+++ Frontend/src/component/Header/Header.js
@@ -120,20 +120,60 @@ const Header = () => {
         </ul>
         {show ? (
           <div className="orgcard">
+           
+            <h5 className="cardtitle" >Organisation Profile &nbsp;
+            <i class="fa fa-times" aria-hidden="true" ></i></h5>
+            
+          
 
-            <h5 className="cardtitle">Organisation Profile</h5>
-            &nbsp;
             <i className="fa fa-building" ></i> &nbsp;
             <a href="/org" className="card-link">
               Orgaisation Profile
             </a>
             
           </div>
-        ) : null}
+          ,
+
+{/* <div className="orgcard card" style={{width: '18rem'}}>
+  <img className="card-img-top" src="../../images/bg1.jpg" alt="Card image " />
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul className="list-group list-group-flush">
+    <li className="list-group-item">Cras justo odio</li>
+    <li className="list-group-item">Dapibus ac facilisis in</li>
+    <li className="list-group-item">Vestibulum at eros</li>
+  </ul>
+  <div className="card-body">
+    <a href="#" className="card-link">Card link</a>
+    <a href="#" className="card-link">Another link</a>
+  </div>
+</div> */}
+
+        ) : null
+        }
       </nav>
-     
+
     </div>
   );
 };
 
 export default Header;
+
+{/* <div className="card" style={{width: '18rem'}}>
+  <img className="card-img-top" src="..." alt="Card image cap" />
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul className="list-group list-group-flush">
+    <li className="list-group-item">Cras justo odio</li>
+    <li className="list-group-item">Dapibus ac facilisis in</li>
+    <li className="list-group-item">Vestibulum at eros</li>
+  </ul>
+  <div className="card-body">
+    <a href="#" className="card-link">Card link</a>
+    <a href="#" className="card-link">Another link</a>
+  </div>
+</div> */}
