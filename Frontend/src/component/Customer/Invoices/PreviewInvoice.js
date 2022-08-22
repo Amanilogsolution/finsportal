c952804e1e5c7517308728a9c2d4a37697b013a2
Amanilogsolution <aman@ilogsolution.com>
2022-08-22 16:26:29 +0530

    DownloadInvoice

--- Frontend/src/component/Customer/Invoices/PreviewInvoice.js
+++ Frontend/src/component/Customer/Invoices/PreviewInvoice.js
@@ -338,7 +338,7 @@ const InvoicePreview = (props) => {
           </div>
           <div className="modal-footer">
             <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" className="btn btn-primary">Save changes</button>
+            {/* <button type="button" className="btn btn-primary">Save changes</button> */}
             <button type="button" className="btn btn-success" onClick={print}>Print</button>
 
           </div>
