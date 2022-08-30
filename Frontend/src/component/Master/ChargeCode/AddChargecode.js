fa547e4b75df5c9940bf6f5844605eb33042fdbf
rupeshilogsolution <rupesh.kumar@ilogsolution.com>
2022-08-30 16:52:33 +0530

    Add ChargeCode Done

--- Frontend/src/component/Master/ChargeCode/AddChargecode.js
+++ Frontend/src/component/Master/ChargeCode/AddChargecode.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Header from "../../Header/Header";
 import Menu from "../../Menu/Menu";
 import Footer from "../../Footer/Footer";
-import { InsertItems, ActiveAccountname, SelectSubAccountname,TotalActiveUnit } from "../../../api";
+import { InsertItems, ActiveAccountname, SelectSubAccountname, TotalActiveUnit } from "../../../api";
 
 
 const AddChargecode = () => {
@@ -19,7 +19,7 @@ const AddChargecode = () => {
             setData(result)
             const result1 = await TotalActiveUnit(localStorage.getItem("Organisation"));
             setUnitdata(result1)
-   
+
         }
         fetchdata()
     }, [])
@@ -65,25 +65,25 @@ const AddChargecode = () => {
         const SACcode = document.getElementById('saccode').value
         const major_code1 = document.getElementById('major_code');
         const major_code = major_code1.options[major_code1.selectedIndex].textContent;
-        const major_code_val=major_code1.value;
+        const major_code_val = major_code1.value;
         const chartofaccount = document.getElementById('chartof_account').value;
         const taxpreference = document.getElementById("taxpreference").value;
-        const Purchase = document.getElementById("item_name_purchase").checked===true?'Purchase':'';
-        const Sales = document.getElementById("item_name_sales").checked===true?'Sales':'';
+        const Purchase = document.getElementById("item_name_purchase").checked === true ? 'Purchase' : '';
+        const Sales = document.getElementById("item_name_sales").checked === true ? 'Sales' : '';
 
         const gstrate = document.getElementById("gstrate").value;
 
         const org = localStorage.getItem('Organisation');
         const user_id = localStorage.getItem('User_id');
 
-        console.log(type,Name,unit,HSNcode,SACcode,major_code,major_code_val,chartofaccount,taxpreference,Purchase,Sales,gstrate)
-        console.log(Purchase,Sales)
+        console.log(type, Name, unit, HSNcode, SACcode, major_code, major_code_val, chartofaccount, taxpreference, Purchase, Sales, gstrate)
+        console.log(Purchase, Sales)
 
-        if (!Name  || !unit || !major_code || !chartofaccount || !taxpreference ) {
+        if (!Name || !unit || !major_code || !chartofaccount || !taxpreference) {
             alert('Please Enter the mandatory field')
         }
         else {
-            const result = await InsertItems(org,type,Name,unit,SACcode,HSNcode,major_code_val,major_code,chartofaccount,taxpreference,Sales,Purchase,gstrate,user_id);
+            const result = await InsertItems(org, type, Name, unit, SACcode, HSNcode, major_code_val, major_code, chartofaccount, taxpreference, Sales, Purchase, gstrate, user_id);
             if (result == "Added") {
                 alert('Data Added')
                 window.location.href = '/ShowChargecode'
@@ -115,13 +115,13 @@ const AddChargecode = () => {
                                     <div className="card" style={{ width: "100%" }}>
                                         <article className="card-body">
                                             <form>
-                                            <div className="form-row" >
-                                                        <label htmlFor="type" className="col-md-2 col-form-label font-weight-normal"  >Type</label>
-                                                        <div className="col form-group "  onChange={handletype} >
-                                                            <input className="col-mt-2" type="radio" id="type" name="itemtype" value='Goods' />  Goods  &nbsp; &nbsp;
-                                                            <input className="col-mt-2" type="radio" id="type" name="itemtype" value='Service' />  Service
-                                                        </div>
+                                                <div className="form-row" >
+                                                    <label htmlFor="type" className="col-md-2 col-form-label font-weight-normal"  >Type</label>
+                                                    <div className="col form-group " onChange={handletype} >
+                                                        <input className="col-mt-2" type="radio" id="type" name="itemtype" value='Goods'  defaultChecked={true}/>  Goods  &nbsp; &nbsp;
+                                                        <input className="col-mt-2" type="radio" id="type" name="itemtype" value='Service' />  Service
                                                     </div>
+                                                </div>
 
                                                 <div className="form-row">
                                                     <label htmlFor="description" className="col-md-2 col-form-label font-weight-normal">Name<span style={{ color: "red" }}>*</span></label>
@@ -130,32 +130,32 @@ const AddChargecode = () => {
                                                     </div>
                                                 </div>
                                                 <div className="form-row" >
-                                                        <label htmlFor="unit" className="col-md-2 col-form-label font-weight-normal " >Unit</label>
-                                                        <div className="col form-group">
-                                                            <select className="form-control col-md-4" id="unit">
-                                                                <option value='' hidden>Select Unit</option>
-                                                                {
-                                                                    unitdata.map((item, index) => (
-                                                                        <option value={item.unit_symbol} key={index} >{item.unit_name}&nbsp;&nbsp;({item.unit_symbol})</option>
+                                                    <label htmlFor="unit" className="col-md-2 col-form-label font-weight-normal " >Unit</label>
+                                                    <div className="col form-group">
+                                                        <select className="form-control col-md-4" id="unit">
+                                                            <option value='' hidden>Select Unit</option>
+                                                            {
+                                                                unitdata.map((item, index) => (
+                                                                    <option value={item.unit_symbol} key={index} >{item.unit_name}&nbsp;&nbsp;({item.unit_symbol})</option>
 
-                                                                    ))
-                                                                }
-                                                            </select>
-                                                        </div>
+                                                                ))
+                                                            }
+                                                        </select>
                                                     </div>
+                                                </div>
 
-                                                    <div className="form-row" id="hsncodetoogle">
-                                                        <label htmlFor="hsncode" className="col-md-2 col-form-label font-weight-normal" >HSN CODE</label>
-                                                        <div className="col form-group">
-                                                            <input className="form-control col-md-4" type="text" id="hsncode" />
-                                                        </div>
+                                                <div className="form-row" id="hsncodetoogle">
+                                                    <label htmlFor="hsncode" className="col-md-2 col-form-label font-weight-normal" >HSN CODE</label>
+                                                    <div className="col form-group">
+                                                        <input className="form-control col-md-4" type="text" id="hsncode" />
                                                     </div>
-                                                    <div className="form-row" id="saccodetoogle" style={{display:"none"}} >
-                                                        <label htmlFor="saccode" className="col-md-2 col-form-label font-weight-normal" >SAC</label>
-                                                        <div className="col form-group">
-                                                            <input className="form-control col-md-4" type="text" id="saccode" />
-                                                        </div>
+                                                </div>
+                                                <div className="form-row" id="saccodetoogle" style={{ display: "none" }} >
+                                                    <label htmlFor="saccode" className="col-md-2 col-form-label font-weight-normal" >SAC</label>
+                                                    <div className="col form-group">
+                                                        <input className="form-control col-md-4" type="text" id="saccode" />
                                                     </div>
+                                                </div>
                                                 {/* <div className="form-row">
                                                     <label htmlFor="nature" className="col-md-2 col-form-label font-weight-normal">Nature<span style={{ color: "red" }}>*</span></label>
                                                     <div className="col form-group">
@@ -178,7 +178,7 @@ const AddChargecode = () => {
                                                     <label htmlFor="chartof_account" className="col-md-2 col-form-label font-weight-normal">Chart of Account<span style={{ color: "red" }}>*</span></label>
                                                     <div className="col form-group">
                                                         <select className="form-control col-md-4" id='chartof_account' >
-                                                            <option value='' hidden>select the major Code</option>
+                                                            <option value='' hidden>Select the Chart of Account</option>
                                                             {
                                                                 chartofaccountlist.map((item, index) =>
                                                                     <option key={index} value={item.account_sub_name}>{item.account_sub_name}</option>)
@@ -187,31 +187,32 @@ const AddChargecode = () => {
                                                     </div>
                                                 </div>
                                                 <div className="form-row" >
-                                                        <label htmlFor="taxpreference" className="col-md-2 col-form-label font-weight-normal " >Tax Preference<span style={{ color: "rgba(210,0,0,0.7)" }}> *</span></label>
-                                                        <div className="col form-group">
-                                                            <select className="form-control col-md-4" id="taxpreference" onChange={handletaxprefrnce}>
-                                                                <option value='' hidden>Select Tax Preference</option>
-                                                                <option value='Taxable' >Taxable</option>
-                                                                <option value='Non-Taxable' >Non-Taxable</option>
-                                                                <option value='Out-of-Scope' >Out of Scope</option>
-                                                                <option value='Non-GST Supply' >Non-GST Supply </option>
+                                                    <label htmlFor="taxpreference" className="col-md-2 col-form-label font-weight-normal " >Tax Preference<span style={{ color: "rgba(210,0,0,0.7)" }}> *</span></label>
+                                                    <div className="col form-group">
+                                                        <select className="form-control col-md-4" id="taxpreference" onChange={handletaxprefrnce}>
+                                                            <option value='' hidden>Select Tax Preference</option>
+                                                            <option value='Taxable' >Taxable</option>
+                                                            <option value='Non-Taxable' >Non-Taxable</option>
+                                                            <option value='Out-of-Scope' >Out of Scope</option>
+                                                            <option value='Non-GST Supply' >Non-GST Supply </option>
 
-                                                            </select>
-                                                        </div>
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+                                                <div className="form-row col-md-6">
+                                                    <div className="form-group d-flex col-md-4 float-left" ></div>
+                                                    <div className="form-group d-flex col-md-2" >
+                                                        <input className="form-control mt-2" type="checkbox" id="item_name_purchase" style={{ height: "16px", width: "16px" }} />
+                                                        <label htmlFor="item_name" className="col col-form-label font-weight-normal">Purchase</label>
                                                     </div>
 
-                                                    <div className="form-row">
-                                                                <div className="form-group " style={{ marginTop: "10px" }} >
-                                                                    <input className="form-control" type="checkbox" id="item_name_purchase" style={{ height: "16px", width: "16px" }} />
-                                                                </div>
-                                                                <label htmlFor="item_name" className="col col-form-label font-weight-normal">Purchase</label>
+                                                    <div className="form-group d-flex ml-5">
+                                                        <input className="form-control mt-2" type="checkbox" id="item_name_sales" style={{ height: "16px", width: "16px" }} />
+                                                        <label htmlFor="item_name" className="col col-form-label font-weight-normal">Sales</label>
+                                                    </div>
 
-                                                                <div className="form-group " style={{ marginTop: "10px" }} >
-                                                                    <input className="form-control" type="checkbox" id="item_name_sales" style={{ height: "16px", width: "16px" }} />
-                                                                </div>
-                                                                <label htmlFor="item_name" className="col col-form-label font-weight-normal">Sales</label>
-
-                                                            </div>
+                                                </div>
                                                 {/* <div className="form-row">
                                                     <label htmlFor="activity" className="col-md-2 col-form-label font-weight-normal">Activity<span style={{ color: "red" }}>*</span></label>
                                                     <div className="col form-group">
@@ -230,13 +231,13 @@ const AddChargecode = () => {
                                                         <input type="number" className="form-control col-md-4" id='gstrate' maxLength={3} />
                                                     </div>
                                                 </div>
-
+                                                <div className="border-top card-body">
+                                                    <button type='submit' className="btn btn-success" onClick={handleClick}>Add</button>
+                                                    <button className="btn btn-light ml-3" onClick={() => { window.location.href = "./ShowChargecode" }}>Cancel</button>
+                                                </div>
                                             </form>
                                         </article>
-                                        <div className="border-top card-body">
-                                            <button className="btn btn-success" onClick={handleClick}>Add</button>
-                                            <button className="btn btn-light ml-3" onClick={() => { window.location.href = "./ShowChargecode" }}>Cancel</button>
-                                        </div>
+
                                     </div>
                                 </div>
                             </div>
