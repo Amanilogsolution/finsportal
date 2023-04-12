e0afa060e33917c6cd22919b0cb77ead4023bb8a
Rupesh <rupesh.kumar@ilogsolution.com>
2023-04-12 10:09:35 +0530

    Dn Preview Done

--- Frontend/src/component/Vendor/VendorCredits/VendorCreditsPreview/VendorCreditsPreview.js
+++ Frontend/src/component/Vendor/VendorCredits/VendorCreditsPreview/VendorCreditsPreview.js
@@ -1,19 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { showOrganisation } from '../../../../api'
-import './CreditNotePreview.css'
+import '../../../Customer/CreditNotes/CreditNotePreview/CreditNotePreview.css'
 
-const CreditNotePreview = ({ ChargeCodeSub, data, location,custname }) => {
+const VendorCreditsPreview = ({ data, Dndata, DebitCodeSub }) => {
     const [orgdata, setOrgdata] = useState([])
     useEffect(() => {
         const fetchdata = async () => {
             let org = localStorage.getItem('Organisation');
             const result = await showOrganisation(org)
             setOrgdata(result)
+            console.log('nuw',DebitCodeSub,DebitCodeSub.length)
         }
         fetchdata()
-    }, [data])
-
-    console.log('nhieh', ChargeCodeSub)
+    }, [DebitCodeSub])
     return (
         <>
             <div className="modal fade bd-example-modal-lg" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -32,81 +31,102 @@ const CreditNotePreview = ({ ChargeCodeSub, data, location,custname }) => {
                                     </span>
                                 </div>
                                 <div className='cn-detailminiPreview d-flex justify-content-between align-items-center px-4'>
-                                    <span><span className='font-weight-bold'>Date:</span> {data.inv_Date} </span>
-                                    <span><span className='font-weight-bold'>Invoice Number:</span> {data.inv_no} </span>
-                                    <span><span className='font-weight-bold'>Amount:</span> {data.net_amt}</span>
+                                    <span><span className='font-weight-bold'>Bill Date:</span> {data.billdate} </span>
+                                    <span><span className='font-weight-bold'>Bill Number:</span> {data.bill_no} </span>
+                                    <span><span className='font-weight-bold'>Amount:</span> {data.total_bill_amt}</span>
                                 </div>
                                 <div className='cn-detailsPreview d-flex'>
                                     <div className='cn-detailsPreview1 '>
                                         <div>
-                                            <span className='font-weight-bold detail-head' >Customer Name: </span>
-                                            <span>{custname}</span>
+                                            <span className='font-weight-bold detail-head' >Vendor Name: </span>
+                                            <span>{data.vend_name}</span>
                                         </div>
                                         <div>
-                                            <span className='font-weight-bold detail-head' >Credit Note: </span>
-                                            <span>{data.cn_no}</span>
+                                            <span className='font-weight-bold detail-head' >Voucher Number : </span>
+                                            <span>{data.vourcher_no}</span>
                                         </div>
                                         <div>
-                                            <span className='font-weight-bold detail-head' >Invoice Date: </span>
-                                            <span>{data.inv_Date}</span>
+                                            <span className='font-weight-bold detail-head' >Debit Note no.: </span>
+                                            <span>{Dndata.dn_no}</span>
+                                        </div>
+                                        <div>
+                                            <span className='font-weight-bold detail-head' >Debit Note Date: </span>
+                                            <span>{Dndata.dn_Date}</span>
                                         </div>
                                     </div>
                                     <div className='cn-detailsPreview2 '>
                                         <div>
                                             <span className='font-weight-bold detail-head' >Location: </span>
-                                            <span>{`${location.location_add1},${location.location_city},${location.location_state},${location.location_country}- ${location.location_pin}`}</span>
+                                            <span>{data.location}</span>
                                         </div>
                                         <div>
-                                            <span className='font-weight-bold detail-head' >Credit Note: </span>
-                                            <span>{data.cn_no}</span>
+                                            <span className='font-weight-bold detail-head' >Voucher Date: </span>
+                                            <span>{data.voudate}</span>
+                                        </div>
+                                        <div>
+                                            <span className='font-weight-bold detail-head' >Debit Note amt.: </span>
+                                            <span>{Dndata.total_dn_amt}</span>
                                         </div>
 
                                     </div>
                                 </div>
-                                <div className='cn-orgPreview-items'>
+                                 <div className='cn-orgPreview-items'>
                                     <table className='table table-bordered border border-danger'>
                                         <thead>
                                             <tr >
-                                                <th className='py-1'>Activity</th>
-                                                <th className='py-1'>Charge Code</th>
+                                                <th className='py-1'>Item</th>
+                                                <th className='py-1'>Gl Code</th>
                                                 <th className='py-1'>Amount</th>
-                                                <th className='py-1'>Amount Balance</th>
+                                                <th className='py-1'>Balance Amount</th>
                                                 <th className='py-1'>Pass Amount</th>
                                                 <th className='py-1'>Remark</th>
                                             </tr>
                                         </thead>
                                         <tbody>
                                             {
-                                                ChargeCodeSub.map((item, index) => (
+                                                DebitCodeSub.length>1?
+                                                DebitCodeSub.map((item, index) => (
                                                     <tr key={index}>
-                                                        <td>{item.activity}</td>
-                                                        <td>{item.item}</td>
-                                                        <td>{item.amount}</td>
+                                                        <td>{item.items}</td>
+                                                        <td>{item.gl_code}</td>
+                                                        <td>{item.amt}</td>
                                                         <td>{item.balance_amt}</td>
                                                         <td>{item.pass_amt}</td>
-                                                        <td>{item.remark}</td>
+                                                        <td>{item.narration}</td>
                                                     </tr>
-                                                ))
+                                                )):null
                                             }
                                         </tbody>
                                     </table>
 
-                                </div>
-                                <div className='cn-detailsTotalPreview'>
+                                </div> 
+                                <div className='cn-detailsTotalPreview' style={{height:'200px'}}>
                                     <div className='cn-detailsTotalPreviewsub h-100 float-right rounded'>
                                         <table className='w-100 m-2'>
                                             <tbody>
                                                 <tr>
                                                     <th><h5>Net Amount: </h5></th>
-                                                    <th>{data.total_amt}</th>
+                                                    <th>{Dndata.net_amt}</th>
                                                 </tr>
                                                 <tr>
-                                                    <th><h5>Credit Note Amount:  </h5></th>
-                                                    <th>{data.total_cn_amt}</th>
+                                                    <th><h5>IGST:  </h5></th>
+                                                    <th>{Dndata.igst_amt}</th>
+                                                </tr>
+                                                <tr>
+                                                    <th><h5>CGST:  </h5></th>
+                                                    <th>{Dndata.cgst_amt}</th>
+                                                </tr>
+                                                <tr>
+                                                    <th><h5>SGST:  </h5></th>
+                                                    <th>{Dndata.sgst_amt}</th>
+                                                </tr>
+                                                <tr>
+                                                    <th><h5>Total GST:  </h5></th>
+                                                    <th>{Dndata.total_gst_amt}</th>
                                                 </tr>
                                                 <tr>
                                                     <th><h5>Total Amount:  </h5></th>
-                                                    <th>{data.net_amt}</th>
+                                                    <th>{Dndata.total_bill_amt}</th>
                                                 </tr>
                                             </tbody>
                                         </table>
@@ -125,4 +145,4 @@ const CreditNotePreview = ({ ChargeCodeSub, data, location,custname }) => {
     )
 }
 
-export default CreditNotePreview;
+export default VendorCreditsPreview;
