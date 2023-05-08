e6b1624f35d0b4e3b2c159e895c1354a6e4f3dd8
Rupesh <rupesh.kumar@ilogsolution.com>
2023-05-08 10:54:20 +0530

    Done

--- Frontend/src/component/Customer/CreditNotes/CreditNotePreview/CreditNotePreview.js
+++ Frontend/src/component/Customer/CreditNotes/CreditNotePreview/CreditNotePreview.js
@@ -25,12 +25,11 @@ const CreditNotePreview = ({ ChargeCodeSub, data, location, custname }) => {
             let org = localStorage.getItem('Organisation');
             const result = await showOrganisation(org)
             setOrgdata(result)
-           
         }
         fetchdata()
     }, [])
-    console.log('yiu',ChargeCodeSub)
-    console.log('nwkjd', data,data.cn_no)
+    console.log('nknlk',data)
+   
     return (
         <>
             <div className="modal fade bd-example-modal-lg" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
