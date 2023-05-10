48b5a458e6d31694589fd4af7af8aeba7b54cb5e
Rupesh <rupesh.kumar@ilogsolution.com>
2023-05-10 16:53:24 +0530

    USerManual Done

--- Frontend/src/component/UserManual/UserManualSidebar.js
+++ Frontend/src/component/UserManual/UserManualSidebar.js
@@ -2,11 +2,11 @@ import React, { useState } from 'react'
 import './UserManualSidebar.css'
 import Customer from './Customer/Customer';
 import Items from './Items/Items';
-import Dashboard from './Dashboard/Dashboard'
 import CustAddress from './CustomerAddress/CustAddress';
+import Dashboard from './Dashboard/Dashboard';
 import CustInvoice from './CustInvoice/CustInvoice';
 import fins from '../../images/finsgrowlogo.png'
-
+import { Link } from 'react-router-dom'
 const UserManualSidebar = () => {
     const [currentStep, setStep] = useState(1);
 
@@ -25,85 +25,84 @@ const UserManualSidebar = () => {
         }
     }
     return (
-        <div>
-            <div className='complete'>
-                <div className='sidebar_div'>
-                    <div className='brand'>
-                        <img src={fins} alt='company logo' />
+        <div className='complete'>
+            <div className='usermanual_sidebar position-fixed'>
+                <div className='usermanual_brand p-2'>
+                    <img src={fins} alt='company logo' />
+                </div>
+                <div className='overflow-auto' style={{ height: " 90vh" }}>
+                    <div className='fields'>
+                        <ul>
+                            <li onClick={() => { setStep(1) }}>
+                                <Link to='home'>Dashboard</Link> </li>
+                        </ul>
                     </div>
-                    <div style={{ overflow: "auto", height: " 90vh" }}>
-                        <div className='fields'>
-                            <ul>
-                                <li onClick={() => { setStep(1) }}>Dashboard</li>
-                            </ul>
+                    <div className='fields'>
+                        <ul>
+                            <li onClick={() => { setStep(2) }}>Items</li>
+                        </ul>
+                    </div>
+                    <div className='fields'>
+                        <div className='d-flex'>
+                            <i className="fa fa-caret-down" aria-hidden="true" style={{ margin: "3px" }} ></i><p >Sales</p>
                         </div>
-                        <div className='fields'>
-                            <ul>
-                                <li onClick={() => { setStep(2) }}>Items</li>
-                            </ul>
+                        <ul id='sales_ul' >
+                            <li onClick={() => { setStep(3) }}>Customer</li>
+                            <li onClick={() => { setStep(4) }}>Address</li>
+                            <li onClick={() => { setStep(5) }}>Invoice</li>
+                        </ul>
+                    </div>
+                    <div className='fields'>
+                        <div className='d-flex'>
+                            <i className="fa fa-caret-down" aria-hidden="true" style={{ margin: "3px" }} ></i>
+                            <p>Purchase</p>
                         </div>
-                        <div className='fields'>
-                            <div className='d-flex'>
-                                <i className="fa fa-caret-down" aria-hidden="true" style={{ margin: "3px" }} ></i><p>Sales</p>
-                            </div>
-                            <ul>
-                                <li onClick={() => { setStep(3) }}>Customer</li>
-                                <li onClick={() => { setStep(4) }}>Address</li>
-                                <li onClick={() => { setStep(5) }}>Invoice</li>
-                            </ul>
+                        <ul>
+                            <li>Vendor</li>
+                            <li>Address</li>
+                            <li>Bill</li>
+                        </ul>
+                    </div>
+                    <div className='fields'>
+                        <div className='d-flex'>
+                            <i className="fa fa-caret-down" aria-hidden="true" style={{ margin: "3px" }} ></i><p>Accountant</p>
                         </div>
-                        <div className='fields'>
-                            <div className='d-flex'>
-                                <i className="fa fa-caret-down" aria-hidden="true" style={{ margin: "3px" }} ></i>
-                                <p>Purchase</p>
-                            </div>
-                            <ul>
-                                <li>Vendor</li>
-                                <li>Address</li>
-                                <li>Bill</li>
-                            </ul>
+                        <ul>
+                            <li>Currency Adjustment</li>
+                            <li>Chart of Accounts</li>
+                        </ul>
+                    </div>
+                    <div className='fields'>
+                        <div className='d-flex'>
+                            <i className="fa fa-caret-down" aria-hidden="true" style={{ margin: "3px" }} ></i><p>Masters</p>
                         </div>
-                        <div className='fields'>
-                            <div className='d-flex'>
-                                <i className="fa fa-caret-down" aria-hidden="true" style={{ margin: "3px" }} ></i><p>Accountant</p>
-                            </div>
-                            <ul>
-                                <li>Currency Adjustment</li>
-                                <li>Chart of Accounts</li>
-                            </ul>
-                        </div>
-                        <div className='fields'>
-                            <div className='d-flex'>
-                                <i className="fa fa-caret-down" aria-hidden="true" style={{ margin: "3px" }} ></i><p>Masters</p>
-                            </div>
-                            <ul>
-                                <li>Country</li>
-                                <li>State</li>
-                                <li>City</li>
-                                <li>Unit</li>
-                                <li>Bank</li>
-                                <li>User</li>
-                                <li>Compliance Type</li>
-                                <li>Payment Terms</li>
-                                <li>Employee Master</li>
-                            </ul>
-                        </div>
-                        <div className='fields'>
-                            <ul>
-                                <li>Reports</li>
-                            </ul>
-                        </div>
+                        <ul>
+                            <li>Country</li>
+                            <li>State</li>
+                            <li>City</li>
+                            <li>Unit</li>
+                            <li>Bank</li>
+                            <li>User</li>
+                            <li>Compliance Type</li>
+                            <li>Payment Terms</li>
+                            <li>Employee Master</li>
+                        </ul>
+                    </div>
+                    <div className='fields'>
+                        <ul>
+                            <li>Reports</li>
+                        </ul>
                     </div>
                 </div>
-                <div className='Content'>
-                    <div className='Document_nav'>
-                        <div className='d-flex justify-content-between'>
-                            <h5 style={{ color: "gray", fontWeight: "600" }}>Fins Documents</h5>
-                            <button style={{ marginTop: "-6px" }} className='btn btn-primary'>Sign In</button>
-                        </div>
+            </div>
+            <div className='usermanual_content position-absolute'>
+                <div className='usermanual_document_nav'>
+                    <div className='d-flex justify-content-between'>
+                        <h5 className='text-secondary' style={{ fontWeight: "600" }}>Fins User Manual</h5>
+                        <button className='btn btn-primary'>Dashboard</button>
                     </div>
-                    {showStep(currentStep)}
                 </div>
+                {showStep(currentStep)}
             </div>
         </div>
     )
