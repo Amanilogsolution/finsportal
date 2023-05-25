706ddc3c21492694c60d2d65a63411470e5133ed
Rupesh <rupesh.kumar@ilogsolution.com>
2023-05-25 09:43:45 +0530

    UserManual Done

--- Frontend/src/component/UserManual/Master/RecFreUserManual/RecFreUserManual.js
+++ Frontend/src/component/UserManual/Master/RecFreUserManual/RecFreUserManual.js
@@ -1,12 +1,48 @@
-
+import MenuImg from '../../../../images/UserManual/salesOrder/so1.png'
+import TotalScrImg from '../../../../images/UserManual/salesOrder/so2.png'
+import AddScrImg from '../../../../images/UserManual/salesOrder/so3.png'
+import UpdateScrImg from '../../../../images/UserManual/Customers/img6.jpg'
+import ImportScrImg from '../../../../images/UserManual/Customers/img3.jpg'
 
 const RecFreUserManual = () => {
     return (
         <>
-            <div className='pt-3'>
-                <div className="container-fluid">
-                    <h3 className="ml-5">Recurring Frequency</h3>
+             <div className='p-4'>
+                <div className='d-flex justify-content-around'>
+                    <div className='m-auto' style={{ width: "70%" }}>
+                        <h2>Recurring Frequency</h2>
+                        <p>The first step is go onto sidebar click onto Master dropdown. You will see options select Recurring Frequency.</p>
+                    </div>
+                    <div className='d-flex justify-content-end' style={{ width: "30%" }}>
+                        <img style={{ width: "140px", height: "350px" }} src={MenuImg} />
+                    </div>
+                </div>
+                <div className=''>
+                    <h2>Total Recurring Frequency</h2>
+                    <p>After click on Recurring Frequency you will see the interface total Recurring Frequency table</p>
+                    <div className='d-flex justify-content-center my-4'>
+                        <img className='border' style={{ width: "720px", height: "320px" }} src={TotalScrImg} />
+                    </div>
+                </div>
 
+                <div className=''>
+                    <div >
+                        <h2>Add new Frequency</h2>
+                        <p>To add new Frequency click on add Frequency button. A new interface will appear you can see below, fill all the mandatory details than save this.</p>
+                    </div>
+                    <div className='d-flex justify-content-center'>
+                        <img className='border' src={AddScrImg} style={{ width: "720px" }} />
+                    </div>
+                </div>
+                <div className='d-flex justify-content-between my-2'>
+                    <div >
+                        <h2>Update Frequency</h2>
+                        <p>If you want to update Frequency we also have option click on the Recurring type at Total Recurring Frequency table,</p>
+                        <p>Fill mandatory Fields and than click on update button to update the Frequency Data</p>
+                    </div>
+                    <div>
+                        <img className='border' src={UpdateScrImg} style={{ width: "300px" }} />
+                    </div>
                 </div>
             </div>
         </>
