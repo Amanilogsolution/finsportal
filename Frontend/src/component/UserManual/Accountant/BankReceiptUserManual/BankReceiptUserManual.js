1f703dcb6daa8ca6f67064625e64282f4f83e679
Rupesh <rupesh.kumar@ilogsolution.com>
2023-06-20 17:03:16 +0530

    User Manual Done

---
+++ Frontend/src/component/UserManual/Accountant/BankReceiptUserManual/BankReceiptUserManual.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import MenuImg from '../../../../images/UserManual/salesOrder/so1.png'
+import TotalScrImg from '../../../../images/UserManual/salesOrder/so2.png'
+import AddScrImg from '../../../../images/UserManual/salesOrder/so3.png'
+import UpdateScrImg from '../../../../images/UserManual/Customers/img6.jpg'
+import ImportScrImg from '../../../../images/UserManual/Customers/img3.jpg'
+
+
+
+const BankReceiptUserManual = () => {
+    return (
+        <>
+            <div className='p-4'>
+                <div className='d-flex justify-content-around'>
+                    <div className='m-auto' style={{ width: "70%" }}>
+                        <h2>Bank Receipt</h2>
+                    </div>
+                    <div className='d-flex justify-content-end' style={{ width: "30%" }}>
+                        <img style={{ width: "140px", height: "350px" }} src={MenuImg} />
+                    </div>
+                </div>
+                <div className=''>
+                    <h2>Total Bank Receipt</h2>
+                    <p>Click on Accountant at Sidebar Menu to show total Bank Receipt in table.</p>
+                    <div className='d-flex justify-content-center my-4'>
+                        <img className='border' style={{ width: "720px", height: "320px" }} src={TotalScrImg} />
+                    </div>
+                </div>
+                <div className=''>
+                    <h2>Add Bank Receipt</h2>
+                    <p>Click the Add Bank Receipt Button, to add a new receipt.</p>
+                    <div className='d-flex justify-content-center my-4'>
+                        <img className='border' style={{ width: "720px", height: "320px" }} src={TotalScrImg} />
+                    </div>
+                </div>
+
+                <div className=''>
+                    <div>
+                        <h2>Edit Bank Receipt</h2>
+                        <p>To Update Bank Receipt click on Bank Receipt id. A new interface will appear you can see below, fill all the mandatory details than save this.</p>
+                    </div>
+                    <div className='d-flex justify-content-center'>
+                        <img className='border' src={AddScrImg} style={{ width: "720px" }} />
+                    </div>
+                </div>
+            
+             
+            </div>
+        </>
+    )
+}
+
+export default BankReceiptUserManual
